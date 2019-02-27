c379c6a563b7d0382e2b34947a9925ee809dab83
gromley111 <gromley111@gmail.com>
2019-02-26 18:51:20 -0800

    first commit

--- client/src/components/other-pages/login.js
+++ client/src/components/other-pages/login.js
@@ -3,11 +3,13 @@ import TextField from 'material-ui/TextField';
 import Checkbox from 'material-ui/Checkbox';
 import RaisedButton from 'material-ui/RaisedButton';
 
-const Login =() => (
+const Login = () => (
   <div>
     <div className="login-wrapper">
       <div className="login-fields">
         <h3>Login <a href="">Forgot Password?</a></h3>
+
+        {/* hjbvguvuv */}
         <TextField
           id="name"
           floatingLabelText="Usernae or Email"
@@ -25,7 +27,7 @@ const Login =() => (
           />
         </div>
         <div className="pt20">
-          <RaisedButton label="Log In" primary={true} fullWidth={true}/>
+          <RaisedButton label="Log In" primary={true} fullWidth={true} />
         </div>
 
       </div>
